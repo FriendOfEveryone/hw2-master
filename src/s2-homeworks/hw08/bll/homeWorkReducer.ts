b07e04bc7f71ed34774840b7dd4c72c74ae86927
Denis <d_sudnikov@mail.ru>
2024-06-10 19:36:55 +0300

    update HW

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort':
+        { // by name
+            const newState = [...state]
+            if (action.payload === 'up') {
 
-            return state // need to fix
+                return [...newState.sort((a, b) => a.name.localeCompare(b.name))] // need to fix
+            } else {
+                return [...newState.sort((a, b) => a.name.localeCompare(b.name)).reverse()]
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state.filter(u => u.age >= 18)]// need to fix
         }
         default:
             return state
